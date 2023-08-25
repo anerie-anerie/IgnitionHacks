28cd7ca4ad35278c7be63e53c6829ecf4b99e97b
Anerie Patel <aneriep@gmail.com>
2023-08-25 19:37:59 -0400

    Initial Commit

---
+++ IgnitionHacks/IgnitionHacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  IgnitionHacksApp.swift
+//  IgnitionHacks
+//
+//  Created by Anerie Patel on 2023-08-25.
+//
+
+import SwiftUI
+
+@main
+struct IgnitionHacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
