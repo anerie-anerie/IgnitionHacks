5aba778f753d2db0fe18a2187cfa23b70f4d5a10
Anerie Patel <aneriep@gmail.com>
2023-08-26 15:14:53 -0400

    new info for handbook

--- IgnitionHacks/chain.swift
+++ IgnitionHacks/chain.swift
@@ -6,10 +6,97 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct chain: View {
+    
+    struct WebView: UIViewRepresentable {
+        let urlString: String
+
+        func makeUIView(context: Context) -> WKWebView {
+            let webView = WKWebView()
+            return webView
+        }
+
+        func updateUIView(_ uiView: WKWebView, context: Context) {
+            if let url = URL(string: urlString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(red: 0.5, green: 0.179, blue: 0.239)
+                .ignoresSafeArea()
+                .overlay(
+           
+                    VStack {
+            Text("Chain (ch)")
+                .font(.custom("Optima-Regular", size: 35))
+                .foregroundColor(Color(red: 1.002, green: 0.923, blue: 0.798))
+                .multilineTextAlignment(.center)
+                .padding([.top, .leading, .trailing])
+                        
+                      
+                        Text("Chains are the foundation for most patterns.")
+                            .multilineTextAlignment(.center)
+                            .font(.custom("Optima-Regular", size: 20))
+                            .foregroundColor(Color(red: 1.002, green: 0.923, blue: 0.798))
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+                        
+                        Text("Written Instructions")
+                            .multilineTextAlignment(.center)
+                            .font(.custom("Optima-Regular", size: 20))
+                            .foregroundColor(Color(red: 1.002, green: 0.923, blue: 0.798))
+                            .multilineTextAlignment(.center)
+                            .padding([.top, .leading, .trailing])
+                        
+                        Text("1. Hold crochet hook in right hand and make a slip knot on hook.")
+                            .multilineTextAlignment(.center)
+                            .font(.custom("Optima-Regular", size: 15))
+                            .foregroundColor(Color(red: 1.002, green: 0.923, blue: 0.798))
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+                        
+                        Text("2. Bring yarn over hook from back to front and grab it with hook.")
+                            .multilineTextAlignment(.center)
+                            .font(.custom("Optima-Regular", size: 15))
+                            .foregroundColor(Color(red: 1.002, green: 0.923, blue: 0.798))
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+                        
+                        Text("3. Draw hooked yarn through slip knot and onto hook.")
+                            .multilineTextAlignment(.center)
+                            .font(.custom("Optima-Regular", size: 15))
+                            .foregroundColor(Color(red: 1.002, green: 0.923, blue: 0.798))
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+                      
+                        
+                        Text("Video Tutorial")
+                            .multilineTextAlignment(.center)
+                        .font(.custom("Optima-Regular", size: 20))
+                        .foregroundColor(Color(red: 1.002, green: 0.923, blue: 0.798))
+                        .multilineTextAlignment(.center)
+                        .padding([.top, .leading, .trailing])
+                        
+                        WebView(urlString: "https://www.youtube.com/watch?v=7v-mssy5rKU")
+                            .frame(width: 300.0, height: 300.0)
+                        
+                        Text("Sources: Fiber Flux and the Craft Yarn Council")
+                            .multilineTextAlignment(.center)
+                        .font(.custom("Optima-Regular", size: 12))
+                        .foregroundColor(Color(red: 1.002, green: 0.923, blue: 0.798))
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal)
+            
+            }
+            )
+        }
+            
     }
 }
 
--- IgnitionHacks/singleCrochet.swift
+++ IgnitionHacks/singleCrochet.swift
@@ -6,10 +6,90 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct singleCrochet: View {
+    
+    struct WebView: UIViewRepresentable {
+        let urlString: String
+
+        func makeUIView(context: Context) -> WKWebView {
+            let webView = WKWebView()
+            return webView
+        }
+
+        func updateUIView(_ uiView: WKWebView, context: Context) {
+            if let url = URL(string: urlString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(red: 0.5, green: 0.179, blue: 0.239)
+                .ignoresSafeArea()
+                .overlay(
+           
+                    VStack {
+            Text("Single Crochet (sc)")
+                .font(.custom("Optima-Regular", size: 35))
+                .foregroundColor(Color(red: 1.002, green: 0.923, blue: 0.798))
+                .multilineTextAlignment(.center)
+                .padding([.top, .leading, .trailing])
+                        
+                      
+                        Text("Single crochet is used for most crochet plushies.")
+                            .multilineTextAlignment(.center)
+                            .font(.custom("Optima-Regular", size: 20))
+                            .foregroundColor(Color(red: 1.002, green: 0.923, blue: 0.798))
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+                        
+                        Text("Written Instructions")
+                            .multilineTextAlignment(.center)
+                            .font(.custom("Optima-Regular", size: 20))
+                            .foregroundColor(Color(red: 1.002, green: 0.923, blue: 0.798))
+                            .multilineTextAlignment(.center)
+                            .padding([.top, .leading, .trailing])
+                        
+                        Text("1. Insert hook into center of next chain stitch. Draw yarn through the chain stitch and up onto the hook. There are now 2 loops on hook.")
+                            .multilineTextAlignment(.center)
+                            .font(.custom("Optima-Regular", size: 15))
+                            .foregroundColor(Color(red: 1.002, green: 0.923, blue: 0.798))
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+                        
+                        Text("2. Bring yarn over hook from back to front, and draw it through both loops on hook. One loop remains on the hook, which is one single crochet stitch.")
+                            .multilineTextAlignment(.center)
+                            .font(.custom("Optima-Regular", size: 15))
+                            .foregroundColor(Color(red: 1.002, green: 0.923, blue: 0.798))
+                            .multilineTextAlignment(.center)
+                            .padding(.horizontal)
+                      
+                        
+                        Text("Video Tutorial")
+                            .multilineTextAlignment(.center)
+                        .font(.custom("Optima-Regular", size: 20))
+                        .foregroundColor(Color(red: 1.002, green: 0.923, blue: 0.798))
+                        .multilineTextAlignment(.center)
+                        .padding([.top, .leading, .trailing])
+                        
+                        WebView(urlString: "https://www.youtube.com/watch?v=7oSOQ_8l6O8&t=10s")
+                            .frame(width: 300.0, height: 300.0)
+                        
+                        Text("Sources: Yarnspirations and the Craft Yarn Council")
+                            .multilineTextAlignment(.center)
+                        .font(.custom("Optima-Regular", size: 12))
+                        .foregroundColor(Color(red: 1.002, green: 0.923, blue: 0.798))
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal)
+            
+            }
+            )
+        }
+            
     }
 }
 
