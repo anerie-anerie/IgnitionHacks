c03936eb4966725e644d93b9d3277dac4d69a122
Anerie Patel <aneriep@gmail.com>
2023-08-25 22:48:05 -0400

    basic setup

---
+++ IgnitionHacks/mainMenu.swift
@@ -0,0 +1,20 @@
+//
+//  mainMenu.swift
+//  IgnitionHacks
+//
+//  Created by Anerie Patel on 2023-08-25.
+//
+
+import SwiftUI
+
+struct mainMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct mainMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        mainMenu()
+    }
+}
