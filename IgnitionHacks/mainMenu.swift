bc4c764a1b3ff1a289d866e84f546658dbd40b14
Anerie Patel <aneriep@gmail.com>
2023-08-25 22:55:16 -0400

    ready

--- IgnitionHacks/mainMenu.swift
+++ IgnitionHacks/mainMenu.swift
@@ -9,7 +9,24 @@ import SwiftUI
 
 struct mainMenu: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(red: 0.5, green: 0.179, blue: 0.239)
+                .ignoresSafeArea()
+                .overlay(
+            VStack {
+                Text("Learning Loops Desk")
+                    .font(.custom("Optima-Regular", size: 30))
+                    .foregroundColor(Color(red: 0.991, green: 0.639, blue: 0.704))
+                    .multilineTextAlignment(.center)
+                   
+                Text("Click on the project you want to complete or book for beginner information.")
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal)
+                font(.custom("Optima-Regular", size: 10))
+                .foregroundColor(Color(red: 0.991, green: 0.639, blue: 0.704))
+            }
+            )
+        }
     }
 }
 
